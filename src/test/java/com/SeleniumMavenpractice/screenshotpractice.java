45400b7d4fb1f8ac9e1a411738eda02b1ba2dea8
yasaswini2579 <yasaswini827@gmail.com>
2026-06-29 17:20:53 +0530

    Updated screenshot practice code

--- src/test/java/com/SeleniumMavenpractice/screenshotpractice.java
+++ src/test/java/com/SeleniumMavenpractice/screenshotpractice.java
@@ -1,10 +1,39 @@
 package com.SeleniumMavenpractice;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 public class screenshotpractice {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		RemoteWebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		
+		//FILE
+//	File src =	driver.getScreenshotAs(OutputType.FILE);
+//	File dest = new File("./Screenshotttt/google.png");
+//	FileUtils.copyFile(src, dest);
+//	driver.close();
+		
+		//Bytes
+//	byte[] src =	driver.getScreenshotAs(OutputType.BYTES);
+//	FileOutputStream fos = new FileOutputStream("./Screenshotttt/google.png");
+//	fos.write(src);
+//	driver.close();
+     //Base64
+	String basescreenshot =	driver.getScreenshotAs(OutputType.BASE64);
+	byte[] src = Base64.getDecoder().decode(basescreenshot);
+	FileOutputStream dest = new FileOutputStream("./Screenshotttt/google1.jpg");
+	dest.write(src);
+	driver.close();
 	}
 
 }
