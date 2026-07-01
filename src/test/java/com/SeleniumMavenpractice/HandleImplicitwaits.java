c4c4eb52b88362d9433a19873677a5a7f31b2ab5
yasaswini2579 <yasaswini827@gmail.com>
2026-07-01 18:22:21 +0530

    update

---
+++ src/test/java/com/SeleniumMavenpractice/HandleImplicitwaits.java
@@ -0,0 +1,21 @@
+package com.SeleniumMavenpractice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandleImplicitwaits {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.hyrtutorials.com/p/waits-demo.html");
+        driver.findElement(By.id("btn1")).click();
+        driver.findElement(By.id("txt1")).sendKeys("Yashuu");
+        driver.close();
+	}
+
+}
